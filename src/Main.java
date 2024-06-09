cd245c1a128ab20f6b2b6a1fea48aa20ccfdf1de
luccafm1 <luccafabricio@gmail.com>
2024-06-09 20:03:17 -0300

    Verificações adicionadas 2

--- src/Main.java
+++ src/Main.java
@@ -4,33 +4,55 @@ import Models.UserLoginInfo;
 import Utils.DatabaseConnection;
 
 import java.sql.SQLException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        try {
+            DatabaseConnection.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+            return; // Sai do programa se não conseguir conectar ao banco de dados
+        }
+
         UserLoginInfo user = null;
+        Scanner sc = new Scanner(System.in);
+
         while (user == null) {
             try {
-                DatabaseConnection.getConnection();
+                user = login(sc);
             } catch (SQLException e) {
+                System.out.println("Erro ao conectar ao banco de dados durante o login: " + e.getMessage());
+                return; // Sai do programa se não conseguir verificar o login
+            } catch (Exception e) {
+                System.out.println("Erro durante o login: " + e.getMessage());
                 e.printStackTrace();
-                System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
             }
+
+            if (user == null) {
+                System.out.println("Usuário não cadastrado. Por favor, tente novamente.");
+            }
+        }
+
+        while (user.getUserType().equals("Admin") || user.getUserType().equals("Seller")) {
             try {
-                user = login();
                 Menu menu = new Menu(user.getUserId(), user.getUserType());
                 menu.exibirMenu();
+            } catch (InputMismatchException e) {
+                System.out.println("Opção inválida. Por favor, tente novamente.");
             } catch (Exception e) {
-                System.out.println("Usuário não cadastrado");
+                System.out.println("Erro no menu.");
+                e.printStackTrace();
             }
         }
     }
 
-    public static UserLoginInfo login() throws SQLException {
+    public static UserLoginInfo login(Scanner sc) throws SQLException {
         LoginDAO loginDAO = new LoginDAO();
 
-        Scanner sc = new Scanner(System.in);
         System.out.printf("\nLOGIN\n=====\n");
         System.out.printf("\nE-mail: ");
         String email = sc.nextLine();
